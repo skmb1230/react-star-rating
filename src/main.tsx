558d9d58bc706a39705a7753e1a4df4ef032c250
skmb1230 <skmb1230@naver.com>
2025-01-25 00:30:41 +0900

    push test

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import ReactDOM from "react-dom/client";
 import StarRating from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(<StarRating onClick={(s) => console.log(s)} />);
+root.render(<StarRating />);
